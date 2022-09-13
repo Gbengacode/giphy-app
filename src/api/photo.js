4222bfebb3e765e0d9af8c4c0c347369a263df03
emmanuel <gbengacode>
2022-09-13 17:57:23 +0100

    Add api call

---
+++ src/api/photo.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const fetchPhoto = async (searchTerm) => {
+  const {
+    data: { data },
+  } = await axios.get(
+    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API}&q=${searchTerm}`
+  );
+  return data;
+};
+
+export default fetchPhoto;
